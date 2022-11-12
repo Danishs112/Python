0ec4716a6f289e6b7bfd53ecbb4dd39afa1b65c6
Danish Soma <danish.soma@karyinfotech.com>
2022-11-12 14:31:32 +0530

    binary search by recursion

--- binary_search.py
+++ binary_search.py
@@ -29,3 +29,35 @@ if result != -1:
     print("Element is present on the array at index", str(result))
 else:
     print("Element is not present on the array")
+
+
+# recursive approach
+
+def binary_search_recursion(array, left, right, element):
+    if right >= left:
+
+        mid = left + (right - left) // 2
+
+        if array[mid] == element:
+            return array[mid]
+
+        elif array[mid] > element:
+            return binary_search_recursion(array, left, mid-1, element)
+
+        else:
+            return binary_search_recursion(array, mid + 1,right, element)
+
+    return -1
+
+
+arr = [2, 3, 4, 5, 6, 7, 8, 9]
+ele = int(input("Enter the element:"))
+
+result = binary_search_recursion(arr,0,len(arr)-1,ele)
+
+if result != -1:
+    print("Element is present on the array at index", str(result))
+else:
+    print("Element is not present on the array")
+
+
